965cb25db43560f5e68006540d2c6808d3a3a7bd
Jonathan <rockyroller2005@gmail.com>
2026-04-23 15:40:37 -0400

    Add project files.

---
+++ ParkingTicketSimulation/ParkingTicketSimulation.cpp
@@ -0,0 +1,29 @@
+// Purpose: Use of Aggregation, use multiple classes that interact with eachother.
+// Author: Jonathan L.
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
