989c6c25a89ae8a8cbe75c6f4f79b51668fb2d4e
Jonathan <rockyroller2005@gmail.com>
2026-04-23 19:46:29 -0400

    * parking meter.cpp done

--- ParkingTicketSimulation/ParkingMeter.cpp
+++ ParkingTicketSimulation/ParkingMeter.cpp
@@ -1,1 +1,13 @@
 #include "ParkingMeter.h"
+
+using namespace std;
+
+ParkingMeter::ParkingMeter(int minutes) {
+
+	minutesPurchased = minutes;
+
+}
+
+int ParkingMeter::getMinutesPurchased() const {
+	return minutesPurchased;
+}
