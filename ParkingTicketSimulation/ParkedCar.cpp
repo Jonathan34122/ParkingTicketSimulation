6dcec3552e2cb40a5ddf161bc5fb0dfade49c031
Jonathan <rockyroller2005@gmail.com>
2026-04-23 15:48:26 -0400

    *create constructor for parked car * include private member variables for parked car *pass variables to constructor parameters

--- ParkingTicketSimulation/ParkedCar.cpp
+++ ParkingTicketSimulation/ParkedCar.cpp
@@ -1,1 +1,10 @@
 #include "ParkedCar.h"
+
+
+ParkedCar::ParkedCar(string make, string model, string color, string license, int minutes) {
+	this->make = make;
+	this->model = model;
+	this->color = color;
+	this->licensceNumber = licensceNumber;
+	this->minutesParked = minutesParked;
+}
--- ParkingTicketSimulation/ParkedCar.h
+++ ParkingTicketSimulation/ParkedCar.h
@@ -1,5 +1,23 @@
-#pragma once
-class ParkedCar
-{
+#ifndef PARKEDCAR_H
+#define PARKEDCAR_H
+
+#include <string>
+using namespace std;
+
+class ParkedCar{
+
+private:
+
+	string make;
+	string model;
+	string color;
+	string licensceNumber;
+	int minutesParked;
+
+public:
+
+	ParkedCar(string make, string model, string color, string license, int minutes);
+
 };
 
+#endif
