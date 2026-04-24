b5877c49f35ec6d3ad1897887b9393e57b011d0a
Jonathan <rockyroller2005@gmail.com>
2026-04-24 13:24:20 -0400

    *Officer.h

--- ParkingTicketSimulation/PoliceOfficer.h
+++ ParkingTicketSimulation/PoliceOfficer.h
@@ -1,14 +1,26 @@
-#pragma once
+#ifndef POLICEOFFICER_H
+#define POLICEOFFICER_H
+
+#include <string>
+#include "ParkedCar.h"
+#include "ParkingMeter.h"
+#include "ParkingTicket.h"
+
+using namespace std;
+
 class PoliceOfficer{
 
+private:
 
+	string name;
+	string badgeNumber;
+	
 
+public:
+	PoliceOfficer(string name, string badgeNumber);
 
-
-
-
-
-
+	ParkingTicket* inspectCar(const ParkedCar& car, const ParkingMeter& meter) const;
 
 };
 
+#endif
